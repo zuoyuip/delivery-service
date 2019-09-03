6a66fbeec30217c4af945f0b5b4bdf0e163e2d23
zuoyu <zuoyuip@qq.com>
2019-09-03 16:03:24 +0800

    取消redis的启动加载缓存，解决缓存超时的BUG

--- src/main/java/org/zuoyu/initializing/RedisCommandLine.java
+++ src/main/java/org/zuoyu/initializing/RedisCommandLine.java
@@ -1,39 +1,39 @@
-package org.zuoyu.initializing;
-
-import java.util.Set;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-import org.zuoyu.service.IDeliveryService;
-
-/**
- * 初始化Redis的缓存.
- *
- * @author zuoyu
- * @program delivery-service
- * @create 2019-08-16 17:58
- **/
-@Component
-public class RedisCommandLine implements CommandLineRunner {
-
-  private final RedisTemplate<String, Object> redisTemplate;
-
-  private final IDeliveryService iDeliveryService;
-
-  public RedisCommandLine(RedisTemplate<String, Object> redisTemplate,
-      IDeliveryService iDeliveryService) {
-    this.redisTemplate = redisTemplate;
-    this.iDeliveryService = iDeliveryService;
-  }
-
-  @Override
-  public void run(String... args) {
-    Set<String> keys = redisTemplate.keys("*");
-    if (keys == null || keys.size() < 1) {
-      iDeliveryService.listDelivery();
-      return;
-    }
-    redisTemplate.delete(keys);
-    iDeliveryService.listDelivery();
-  }
-}
+//package org.zuoyu.initializing;
+//
+//import java.util.Set;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Component;
+//import org.zuoyu.service.IDeliveryService;
+//
+///**
+// * 初始化Redis的缓存.
+// *
+// * @author zuoyu
+// * @program delivery-service
+// * @create 2019-08-16 17:58
+// **/
+//@Component
+//public class RedisCommandLine implements CommandLineRunner {
+//
+//  private final RedisTemplate<String, Object> redisTemplate;
+//
+//  private final IDeliveryService iDeliveryService;
+//
+//  public RedisCommandLine(RedisTemplate<String, Object> redisTemplate,
+//      IDeliveryService iDeliveryService) {
+//    this.redisTemplate = redisTemplate;
+//    this.iDeliveryService = iDeliveryService;
+//  }
+//
+//  @Override
+//  public void run(String... args) {
+//    Set<String> keys = redisTemplate.keys("*");
+//    if (keys == null || keys.size() < 1) {
+//      iDeliveryService.listDelivery();
+//      return;
+//    }
+//    redisTemplate.delete(keys);
+//    iDeliveryService.listDelivery();
+//  }
+//}
